b2ae118f20c6f0444172d8ff47b92b18c4eddf07
tamimjdd <deviltamim88@gmail.com>
2022-03-31 15:19:17 +0600

    first commit

---
+++ dist/config/keys.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+exports.default = {
+    mongoURI: 'mongodb://localhost:27017/test',
+};
+//# sourceMappingURL=keys.js.map
---
+++ src/config/keys.ts
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+export default {
+  mongoURI: 'mongodb://localhost:27017/test',
+};
